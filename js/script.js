93f34063185752fb175099517b27fea477546ab6
Kaushik Chemburkar <attherate@gmail.com>
2022-06-10 09:08:31 +1000

    speech synth way 2

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ btn.addEventListener('click', () => {
 
 // let utterance = new SpeechSynthesisUtterance("Hello world!");
 // speechSynthesis.speak(utterance);
+
+// function speak(message) {
+//   var msg = new SpeechSynthesisUtterance(message);
+//   var voices = window.speechSynthesis.getVoices();
+//   msg.voice = voices[0];
+//   window.speechSynthesis.speak(msg);
+// }
+// speak("Hello, world");
+// speak("Good Morning!")
